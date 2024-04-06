c98b91c180ab0d0fd04789c06bfc4f8093856385
Fedorova Maria <49729545+ixxxar@users.noreply.github.com>
2024-04-06 22:43:25 +0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,50 @@
+def factorial(n):
+    if n < 0:
+        return "Факториал отрицательного числа не определен"
+    elif n == 0 or n == 1:
+        return 1
+    else:
+        result = 1
+        for i in range(2, n+1):
+            result *= i
+        return result
+
+number = int(input("Введите число: "))
+print(f"Факториал числа {number} равен {factorial(number)}")
+
+
+def factorial2(n):
+    if n < 0:
+        return "Факториал отрицательного числа не определен"
+    elif n == 0 or n == 1:
+        return 1
+    else:
+        result = 1
+        for i in range(2, n+1):
+            result *= i
+        return result
+
+number = int(input("Введите число: "))
+
+def is_prime(n):
+    if n <= 1:
+        return False
+    elif n == 2:
+        return True
+    elif n % 2 == 0:
+        return False
+
+    i = 3
+    while i*i <= n:
+        if n % i == 0:
+            return False
+        i += 2
+
+    return True
+
+
+number = int(input("Введите число: "))
+if is_prime(number):
+   print(f"{number} - простое число")
+else:
+   print(f"{number} - не является простым числом")
