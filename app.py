59c9185958312f8cbbf1abc8a06f1d87df8927df
Fedorova Maria <49729545+ixxxar@users.noreply.github.com>
2024-04-07 00:17:48 +0500

    Update app.py

--- app.py
+++ app.py
@@ -291,6 +291,147 @@ else:
    print(f"{number} - не является простым числом")
 
 
+def is_p2rime4(n):
+    if n <= 1:
+        return False
+    elif n == 2:
+        return True
+    elif n % 2 == 0:
+        return False
+
+    i = 3
+    while i*i <= n:
+        if n % i == 0:
+            return False
+        i += 2
+
+    return True
+
+
+number = int(input("Введите число: "))
+if is_prime(number):
+   print(f"{number} - простое число")
+else:
+   print(f"{number} - не является простым числом")
+def is_prim2e4(n):
+    if n <= 1:
+        return False
+    elif n == 2:
+        return True
+    elif n % 2 == 0:
+        return False
+
+    i = 3
+    while i*i <= n:
+        if n % i == 0:
+            return False
+        i += 2
+
+    return True
+
+
+number = int(input("Введите число: "))
+if is_prime(number):
+   print(f"{number} - простое число")
+else:
+   print(f"{number} - не является простым числом")
+
+
+
+
+def is_p2rime4(n):
+    if n <= 1:
+        return False
+    elif n == 2:
+        return True
+    elif n % 2 == 0:
+        return False
+
+    i = 3
+    while i*i <= n:
+        if n % i == 0:
+            return False
+        i += 2
+
+    return True
+
+
+number = int(input("Введите число: "))
+if is_prime(number):
+   print(f"{number} - простое число")
+else:
+   print(f"{number} - не является простым числом")
+def is_prim2e4(n):
+    if n <= 1:
+        return False
+    elif n == 2:
+        return True
+    elif n % 2 == 0:
+        return False
+
+    i = 3
+    while i*i <= n:
+        if n % i == 0:
+            return False
+        i += 2
+
+    return True
+
+
+number = int(input("Введите число: "))
+if is_prime(number):
+   print(f"{number} - простое число")
+else:
+   print(f"{number} - не является простым числом")
+
+
+
+def is_p2rime4(n):
+    if n <= 1:
+        return False
+    elif n == 2:
+        return True
+    elif n % 2 == 0:
+        return False
+
+    i = 3
+    while i*i <= n:
+        if n % i == 0:
+            return False
+        i += 2
+
+    return True
+
+
+number = int(input("Введите число: "))
+if is_prime(number):
+   print(f"{number} - простое число")
+else:
+   print(f"{number} - не является простым числом")
+def is_prim2e4(n):
+    if n <= 1:
+        return False
+    elif n == 2:
+        return True
+    elif n % 2 == 0:
+        return False
+
+    i = 3
+    while i*i <= n:
+        if n % i == 0:
+            return False
+        i += 2
+
+    return True
+
+
+number = int(input("Введите число: "))
+if is_prime(number):
+   print(f"{number} - простое число")
+else:
+   print(f"{number} - не является простым числом")
+
+
 def factoria2l(n):
     if n < 0:
         return "Факториал отрицательного числа не определен"
